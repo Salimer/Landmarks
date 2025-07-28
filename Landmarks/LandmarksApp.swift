f722a3c6e41acd22236f93b2c56440e77eda6485
Salim-Bamahfoodh <salim.omer@jisr.net>
2025-07-28 09:41:28 +0300

    project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Salim on 27/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
