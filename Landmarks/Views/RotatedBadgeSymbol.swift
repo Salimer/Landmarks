f7064ee8946e2f4c52463d38d753065acd23d312
Salim-Bamahfoodh <salim.omer@jisr.net>
2025-08-03 12:17:56 +0300

    Draw badge symbol

---
+++ Landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,21 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Salim on 03/08/2025.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
