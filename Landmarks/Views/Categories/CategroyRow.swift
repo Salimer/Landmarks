1a4a4bbf182d6eb7d16773ccd773c98d9320bb72
Salim-Bamahfoodh <salim.omer@jisr.net>
2025-08-05 11:58:16 +0300

    Create category row

---
+++ Landmarks/Views/Categories/CategroyRow.swift
@@ -0,0 +1,39 @@
+//
+//  CategroyRow.swift
+//  Landmarks
+//
+//  Created by Salim on 05/08/2025.
+//
+
+import SwiftUI
+
+struct CategroyRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+            .frame(height: 185)
+        }
+    }
+}
+
+#Preview {
+    let landmarks = ModelData().landmarks
+    CategroyRow(
+        categoryName: landmarks[0].category.rawValue,
+        items: Array(landmarks.prefix(3))
+    )
+}
