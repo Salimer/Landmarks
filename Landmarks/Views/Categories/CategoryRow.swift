52940bcf4c82d405a2e05353560c56b739bd376d
Salim-Bamahfoodh <salim.omer@jisr.net>
2025-08-05 12:52:47 +0300

    Composing complex interfaces

--- Landmarks/Views/Categories/CategoryRow.swift
+++ Landmarks/Views/Categories/CategoryRow.swift
@@ -21,7 +21,11 @@ struct CategoryRow: View {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .top, spacing: 0) {
                     ForEach(items) { landmark in
-                        CategoryItem(landmark: landmark)
+                        NavigationLink {
+                            LandmarkDetail(landmark: landmark)
+                        } label: {
+                            CategoryItem(landmark: landmark)
+                        }
                     }
                 }
             }
@@ -34,6 +38,6 @@ struct CategoryRow: View {
     let landmarks = ModelData().landmarks
     CategoryRow(
         categoryName: landmarks[0].category.rawValue,
-        items: Array(landmarks.prefix(3))
+        items: Array(landmarks.prefix(4))
     )
 }
